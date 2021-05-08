eab610ab6eb36aa07ddf0a29c8e42c31da2fe89d
Annie <ikram.annie0@gmail.com>
2021-05-07 22:42:18 -0400

    edited synopsis of each series, edited css file, started api documention file

--- series.js
+++ series.js
@@ -12,21 +12,21 @@ const series = [{
     network: 'Showtime',
     title: 'Penny Dreadful',
     createdBy: 'John Logan',
-    synopsis: 'A British-American horror drama series ',
+    synopsis: 'A British-American horror drama series. It follows deeply Catholic clairvoyant, Vanessa Ives, retired colonial-African explorer, Sir Malcolm Murray, and American sharpshooter Ethan Chandler, in a supernatural mystery that brings them into conflict and connection with a number of famous literary figures from 19thcentury Gothic fiction. This includes Victor Frankenstein, Dorian Gray, Henry Jekyll, and much of the cast of Bram Stoker’s Dracula.',
     seasons: '3',
 }, {
     id: '3',
     network: 'Netflix',
     title: 'The Haunting of Hill House',
     createdBy: 'Mike Flanagan',
-    synopsis: 'An American supernatural horror drama series loosely based on the 1959 novel of the same name by Shirley Jackson',
+    synopsis: 'An American supernatural horror drama series loosely based on the 1959 novel of the same name by Shirley Jackson. The story follows Hugh and Olivia Crain and their five children as they move into the Hill House. The Crains arrive at Hill House hoping to fix it up and sell it, but things don’t go exactly as planned. They are stuck there longer than expected and the family is subjected to the hauntings.',
     seasons: '1',
 }, {
     id: '4',
     network: 'HBO',
     title: 'Sharp Objects',
     createdBy: 'Marti Noxon',
-    synopsis: 'An American supernatural horror drama series loosely based on the 1959 novel of the same name by Shirley Jackson',
+    synopsis: 'An American psychological thriller miniseries based on a novel of the same name by Gillian Flynn. It follows Camille Preaker, an emotionally troubled reporter who returns to her hometown to cover the murders of two young girls.',
     seasons: '1',
    
 }]
